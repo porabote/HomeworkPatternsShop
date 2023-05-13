f743cd172cf08cbd4519d17875dd953fb6e4ebf7
maksimov <dmaksimov@vtb.ru>
2023-05-13 12:44:27 +0300

    auth

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,19 @@ import java.util.Scanner;
 
 public class Main {
 
+
+    //    1 (S)  https://github.com/porabote/HomeworkPatternsShop/blob/main/src/main/java/user/User.java
+//  (Auth.auth(user); аутентификацией занимается отдельный компонент)
+//
+// 2 (O) https://github.com/porabote/HomeworkPatternsShop/blob/main/src/main/java/components/auth/Auth.java (может приходить любой пользователь реализующий соотв интерфейс)
+//
+// 3 (L) https://github.com/porabote/HomeworkPatternsShop/blob/main/src/main/java/delivery/DeliveryServiceCourier.java - доставка курьером является частным случаем доставки
+//
+// 4 (I) https://github.com/porabote/HomeworkPatternsShop/tree/main/src/main/java/delivery - разделение интерфейсов на доставку и самовывоз (без обязательного указания адреса доставки)
+//
+//  5 (D) вот здесь вопрос, судя по лекции,  это  же можно подвести под тоже что и описано в пункте 2 (O), по примерам указанным в видео непонятно чем принцип открытости-закрытости отличается он этого принципа
+
+
     static Scanner scan = new Scanner(System.in);
     static boolean isProductsSelected = false;
     static HashMap<Integer, Product> products;
@@ -29,23 +42,22 @@ public class Main {
 //        ArrayList catsList = categories.getList();
 
 
-
         createOrder(user, card.getCardList());
 
     }
 
     static private void chooseProducts(Card card) {
-        while(!isProductsSelected) {
+        while (!isProductsSelected) {
 
             System.out.println("Enter products ID and Count for put it in card:");
-            for(Map.Entry<Integer, Product> entry : products.entrySet()) {
+            for (Map.Entry<Integer, Product> entry : products.entrySet()) {
                 Product value = entry.getValue();
                 System.out.println(value);
             }
             System.out.println("If you are done, for go to cart, please, enter 100");
 
             int productId = scan.nextInt();
-            if(productId == 100) {
+            if (productId == 100) {
                 isProductsSelected = true;
                 continue;
             }
